66aed00b5c07571fa0e254e173c551344f21f9d1
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2024-01-20 22:32:33 +0530

    Implement method to add variables to symbol table

--- compiler/symbol_table.py
+++ compiler/symbol_table.py
@@ -17,24 +17,36 @@ class SymbolTable:
 
     Properties:
         symbol_table: dictionary used as the symbol table data structure
-        index_counts: dictionary with the index count of each variable kind
+        index: dictionary with the index count of each variable kind
 
     Methods:
         reset() -> None
+        define(str, str, VariableKind) -> None
     """
 
     def __init__(self):
         self.symbol_table = {}
-        self.index_counts = {}
+        self.index = {}
 
     def reset(self):
         """Empty the symbol table, and reset the four indexes to 0.
         Should be called when starting to compile a subroutine declaration.
         """
         self.symbol_table = {}
-        self.index_counts = {
+        self.index = {
             VariableKind.STATIC: 0,
             VariableKind.FIELD: 0,
             VariableKind.ARG: 0,
             VariableKind.VAR: 0,
         }
+
+    def define(self, name, data_type, kind):
+        """Define (add to the table) a new variable of the given
+        name, type, and kind. Assign to it the index value of that kind,
+        and add 1 to the index."""
+        self.symbol_table["name"] = {
+            "type": data_type,
+            "kind": kind,
+            "index": self.index[kind]
+        }
+        self.index[kind] += 1
