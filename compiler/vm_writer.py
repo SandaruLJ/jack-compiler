3b6e835579e03bc13b06db8f86b7aa20c2b6ffef
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2024-01-26 22:29:20 +0530

    Add new line at the end of each vm command

--- compiler/vm_writer.py
+++ compiler/vm_writer.py
@@ -31,39 +31,39 @@ class VmWriter:
 
     def write_push(self, segment, index):
         """Write a VM push command"""
-        self.output.write(f'push {segment.value} {index}')
+        self.output.write(f'push {segment.value} {index}\n')
 
     def write_pop(self, segment, index):
         """Write a VM pop command"""
-        self.output.write(f'pop {segment.value} {index}')
+        self.output.write(f'pop {segment.value} {index}\n')
 
     def write_arithmetic(self, command):
         """Write a VM arithmetic-logical command"""
-        self.output.write(command.value)
+        self.output.write(f'{command.value}\n')
 
     def write_label(self, label):
         """Write a VM label command"""
-        self.output.write(f'label {label}')
+        self.output.write(f'label {label}\n')
 
     def write_goto(self, label):
         """Write a VM goto command"""
-        self.output.write(f'goto {label}')
+        self.output.write(f'goto {label}\n')
 
     def write_if(self, label):
         """Write a VM if-goto command"""
-        self.output.write(f'if-goto {label}')
+        self.output.write(f'if-goto {label}\n')
 
     def write_call(self, name, num_args):
         """Write a VM call command"""
-        self.output.write(f'call {name} {num_args}')
+        self.output.write(f'call {name} {num_args}\n')
 
     def write_function(self, name, num_vars):
         """Write a VM function command"""
-        self.output.write(f'function {name} {num_vars}')
+        self.output.write(f'function {name} {num_vars}\n')
 
     def write_return(self):
         """Write a VM return command"""
-        self.output.write('return')
+        self.output.write('return\n')
 
     def close(self):
         """Close the output file"""
